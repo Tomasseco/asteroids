952939f9fff27c1d251c9c055708543d30807a17
Tomás Seco Cuenca <tomasseco2000@gmail.com>
2025-05-08 05:24:39 +0200

    Marcadores en asteroides, varios impactos por asteroides, musica, explosiones, efectos de sonido

--- src/app/scenes/game.scene.ts
+++ src/app/scenes/game.scene.ts
@@ -1,5 +1,12 @@
 import Phaser from "phaser";
 
+class Asteroid extends Phaser.Physics.Arcade.Image {
+    impactsLeft: number = 5;
+    marker?: Phaser.GameObjects.Graphics;
+    impactText?: Phaser.GameObjects.Text;
+    blinkTween?: Phaser.Tweens.Tween;
+}
+
 export class GameScene extends Phaser.Scene {
 
     private spaceship!: Phaser.Physics.Arcade.Image;
@@ -11,15 +18,22 @@ export class GameScene extends Phaser.Scene {
     private movingRight = false;
     private shooting = false;
     private lastShotTime = 0;
-    private shotCooldown = 400;
+    private shotCooldown = 180;
+    private controlsEnabled = true;
+    private isPaused = false;
 
     private score = 0;
     private livesDead = 0;
-    private textScore!: Phaser.GameObjects.BitmapText;
+    private textRetro!: Phaser.GameObjects.BitmapText;
     private lives!: Phaser.GameObjects.Sprite;
+    private pauseOverlayText!: Phaser.GameObjects.BitmapText;
 
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
     private spaceKey!: Phaser.Input.Keyboard.Key;
+    private pauseKey!: Phaser.Input.Keyboard.Key;
+
+    private asteroidEvent!: Phaser.Time.TimerEvent;
+
 
     constructor() {
         super({ key: "GameScene" });
@@ -33,11 +47,20 @@ export class GameScene extends Phaser.Scene {
         this.load.image("background", "assets/background.jpg");
         this.load.image("spaceship", "assets/spaceship.png");
         this.load.image("guns", "assets/guns.png");
+        this.load.spritesheet("explosion_sprite", "assets/explosion.png", {
+            frameWidth: 190,
+            frameHeight: 190
+        });
         this.load.spritesheet("lives", "assets/lives.png", {
             frameWidth: 138,
             frameHeight: 56
         });
         this.load.image("knighthawks", "assets/fonts/retro/knight3.png");
+
+        // Sonidos
+        this.load.audio('backgroundMusic', 'assets/sounds/music.mp3');
+        this.load.audio('explosion', 'assets/sounds/explosion.mp3');
+        this.load.audio('impactguns', 'assets/sounds/impactguns.mp3');
     }
 
     create() {
@@ -47,6 +70,14 @@ export class GameScene extends Phaser.Scene {
         const background = this.add.image(width / 2, height / 2, "background");
         background.setScale(Math.max(width / background.width, height / background.height));
         background.setScrollFactor(0).setDepth(0);
+
+        // Música de fondo
+        const music = this.sound.add('backgroundMusic', {
+            loop: true,
+            volume: 0.5
+        });
+
+        music.play();
 
         // Fuente bitmap
         const fontConfig = {
@@ -59,11 +90,23 @@ export class GameScene extends Phaser.Scene {
             "offset.y": 0,
             "spacing.x": 0,
             "spacing.y": 0,
-            lineSpacing: 0
+            lineSpacing: 5
         };
         this.cache.bitmapFont.add('knighthawks', Phaser.GameObjects.RetroFont.Parse(this, fontConfig));
 
-        this.textScore = this.add.bitmapText(20, 20, 'knighthawks', 'SCORE\n0', 20).setDepth(3);
+        this.textRetro = this.add.bitmapText(20, 20, 'knighthawks', 'SCORE\n0', 20).setDepth(3);
+
+        // Animación de explosión
+        this.anims.create({
+            key: "explosion",
+            frames: this.anims.generateFrameNumbers("explosion_sprite", {
+                start: 0,
+                end: 4 
+            }),
+            frameRate: 30,
+            repeat: 0,
+            yoyo: true 
+        });
 
         // Nave
         this.spaceship = this.physics.add.image(width / 2, height - 130, "spaceship");
@@ -97,33 +140,37 @@ export class GameScene extends Phaser.Scene {
         }
         if (this.input.keyboard) {
             this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+            this.pauseKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
         } else {
             console.warn("Keyboard input is not available.");
         }
 
+
+        this.pauseOverlayText = this.add.bitmapText(
+            this.cameras.main.width / 2,
+            this.cameras.main.height / 2,
+            'knighthawks',
+            'PAUSA',
+            50
+        ).setOrigin(0.5).setDepth(10).setVisible(false);
+
         // Grupo de balas
         this.bullets = this.physics.add.group();
 
-        // Clase extendida para asteroides
-        class Asteroid extends Phaser.Physics.Arcade.Image {
-            impactsLeft: number = 5;
-        }
-
-        // Grupo de asteroides
+         // Grupo de asteroides
         this.asteroids = this.physics.add.group({ classType: Asteroid, runChildUpdate: true });
 
         // Generación de asteroides
-        this.time.addEvent({
+        this.asteroidEvent = this.time.addEvent({
             delay: 1000,
             loop: true,
             callback: () => {
                 const x = Phaser.Math.Between(0, width);
                 const asteroid = this.asteroids.create(x, -80, "asteroid") as Asteroid;
-
+        
                 const scale = Phaser.Math.FloatBetween(0.4, 0.8);
                 asteroid.setScale(scale);
-                asteroid.impactsLeft = 5;
-
+        
                 const baseRadius = 80;
                 const radius = baseRadius * scale;
                 asteroid.setCircle(
@@ -131,7 +178,7 @@ export class GameScene extends Phaser.Scene {
                     asteroid.width * scale / 2 - radius,
                     asteroid.height * scale / 2 - radius
                 );
-
+        
                 asteroid.setVelocity(Phaser.Math.Between(-40, 40), Phaser.Math.Between(100, 200));
                 asteroid.setAngularVelocity(Phaser.Math.Between(-100, 100));
                 asteroid.setBounce(1);
@@ -144,46 +191,153 @@ export class GameScene extends Phaser.Scene {
         this.physics.add.collider(this.asteroids, this.asteroids);
 
         this.physics.add.overlap(this.spaceship, this.asteroids, (ship, asteroidObj) => {
-            const asteroid = asteroidObj as Asteroid;
             this.livesDead++;
             this.lives.setFrame(this.livesDead);
-            asteroid.destroy();
+            
+            // Desactivar controles
+            this.controlsEnabled = false;    
+            
+            // Sonido de explosión
+            const explosionSound = this.sound.add('explosion');
+            explosionSound.play();
 
-            this.physics.pause();
-            this.anims.pauseAll();
+            // Elimina marcador y texto del asteroide que impactó la nave
+            const impacted = asteroidObj as Asteroid;
+            impacted.marker?.destroy();
+            impacted.impactText?.destroy();
 
-            const msg = this.add.text(width / 2, height / 2, "¡Has sido destruido!", {
-                fontSize: "30px",
-                color: "#ff0000",
-                fontStyle: "bold",
-                backgroundColor: "#00000066",
-                padding: { x: 20, y: 10 }
-            }).setOrigin(0.5).setDepth(10);
+            const spaceship = ship as Phaser.Physics.Arcade.Image;
+            let explosion = this.add.sprite(spaceship.x, spaceship.y, "explosion_sprite");
+            explosion.play("explosion");
+            explosion.setDepth(11);
+            explosion.setScale(0.8)
+            explosion.on("animationcomplete", () => {
 
-            if (this.livesDead > 3) {
+                this.physics.pause();
+                this.anims.pauseAll();
+                explosion.destroy();
+            });
+
+            asteroidObj.destroy();
+
+          
+            const message = this.add.bitmapText(width / 2, height / 2, 'knighthawks', 'CUIDADIN !!!', 30).setOrigin(0.5).setDepth(10);
+            message.setDropShadow(2, 2, 0x000000, 1);
+            
+            const messageLives = this.add.bitmapText(width / 2, height / 2 + 40, 'knighthawks', 'VIDAS: ' + (3 - this.livesDead), 30).setOrigin(0.5).setDepth(10);
+        
+
+            if (this.livesDead > 2) {
                 this.time.delayedCall(2000, () => this.scene.start("GameOverScene"));
             } else {
                 this.time.delayedCall(5000, () => {
-                    msg.destroy();
+                    // Eliminar marcas y textos de todos los asteroides
+                    this.asteroids.getChildren().forEach((ast) => {
+                        const a = ast as Asteroid;
+                        a.marker?.destroy();
+                        a.impactText?.destroy();
+                    });
+            
+                    // Limpiar asteroides
                     this.asteroids.clear(true, true);
+            
+                    // Reset de nave
                     this.spaceship.setPosition(width / 2, height - 130);
                     this.physics.resume();
                     this.anims.resumeAll();
+                    message.destroy();
+                    messageLives.destroy();
+                    // Reanudar controles
+                    this.controlsEnabled = true;
                 });
             }
         });
 
         this.physics.add.overlap(this.bullets, this.asteroids, (bullet, asteroidObj) => {
-            const asteroid = asteroidObj as Asteroid;
+            const asteroid = asteroidObj as Asteroid & {
+                marker?: Phaser.GameObjects.Graphics,
+                impactText?: Phaser.GameObjects.Text
+            };
+        
+            const bulletImpact = bullet as Phaser.GameObjects.Graphics;
+            
+            // Sonido de impacto
+            const impactSound = this.sound.add('impactguns');
+            impactSound.setVolume(0.3); 
+            impactSound.play();
+
+            let explosion = this.add.sprite(bulletImpact.x, bulletImpact.y, "explosion_sprite");
+            explosion.play("explosion");
+            explosion.setDepth(11);
+            explosion.setScale(0.2);
             bullet.destroy();
             asteroid.impactsLeft--;
+            
+            // Aumentamos el marcador por impacto
+            this.score += 2;
+            
+            const fadingText = this.add.text(asteroid.x, asteroid.y, '+2', {
+                font: '16px Arial',
+                color: "#00ff00"
+            }).setOrigin(0.5).setDepth(10);
+            
+            // Animar para desvanecer el texto
+            this.tweens.add({
+                targets: fadingText,
+                alpha: 0,
+                duration: 2000,
+                ease: 'Linear',
+                onComplete: () => {
+                    fadingText.destroy();
+                }
+            });
+            
+            explosion.on("animationcomplete", () => {
+                
+                explosion.destroy();
+            });
 
+            
+            this.textRetro.setText("SCORE\n" + this.score);
+
+            // Si es el primer impacto, creamos marcador y texto
+            if (!asteroid.marker) {
+                const marker = this.add.graphics();
+                marker.lineStyle(2, 0x00ff00, 1);
+                marker.setDepth(10);
+                asteroid.marker = marker;
+        
+                const impactText = this.add.text(0, 0, '', {
+                    font: "16px Arial",
+                    color: "#00ff00",
+                    padding: { x: 4, y: 2 }
+                }).setOrigin(0.5).setDepth(11);
+                asteroid.impactText = impactText;
+            }
+        
+            // Si impactos se agotan, eliminamos todo
             if (asteroid.impactsLeft <= 0) {
+                
+                // Sonido de explosión
+                const explosionSound = this.sound.add('explosion');
+                explosionSound.play();
+
+                asteroid.marker?.destroy();
+                asteroid.impactText?.destroy();
                 asteroid.destroy();
                 this.score += 10;
-                this.textScore.setText(`SCORE\n${this.score}`);
+
+                let explosion = this.add.sprite(asteroid.x, asteroid.y, "explosion_sprite");
+                explosion.play("explosion");
+                explosion.setDepth(11);
+                explosion.setScale(0.8);
+
+                explosion.on("animationcomplete", () => {
+                    explosion.destroy();
+                });
             }
         });
+        
 
         // Eliminar objetos fuera del mundo
         this.physics.world.on('worldbounds', (body: Phaser.Physics.Arcade.Body) => {
@@ -194,6 +348,30 @@ export class GameScene extends Phaser.Scene {
     }
 
     override update(time: number) {
+        if (!this.controlsEnabled) return;
+       
+        if (Phaser.Input.Keyboard.JustDown(this.pauseKey)) {
+            this.isPaused = !this.isPaused;
+        
+            if (this.isPaused) {
+                this.physics.world.pause();
+                this.anims.pauseAll();
+                this.asteroidEvent.paused = true; 
+                this.pauseOverlayText.setVisible(true);
+            } else {
+                this.physics.world.resume();
+                this.anims.resumeAll();
+                this.asteroidEvent.paused = false; 
+                this.pauseOverlayText.setVisible(false);
+            }
+        }
+        
+        // Si está en pausa, no actualizar nada más
+        if (this.isPaused) {
+            return;
+        }
+        
+        
         const moveStep = this.stepsPositionX / 5;
 
         if (this.movingLeft || this.cursors.left.isDown) {
@@ -208,5 +386,72 @@ export class GameScene extends Phaser.Scene {
             bullet.setCollideWorldBounds(false);
             this.lastShotTime = time;
         }
+        
+        this.asteroids.getChildren().forEach((asteroidObj) => {
+            const asteroid = asteroidObj as Asteroid & {
+                marker?: Phaser.GameObjects.Graphics,
+                impactText?: Phaser.GameObjects.Text
+            };
+             
+            if (asteroid.marker) {
+                const g = asteroid.marker;
+                g.clear();
+                       
+                const size = 15;
+                const halfW = asteroid.displayWidth / 2;
+                const halfH = asteroid.displayHeight / 2;
+                const x = asteroid.x;
+                const y = asteroid.y;
+                
+                let lineColor = 0x00ff00;
+                // Si el asteroide está muy perjudicado, parpadeamos en rojo
+                if (asteroid.impactsLeft <3) {
+                    asteroid.impactText?.setColor("#ff0000");
+                    lineColor = 0xff0000;
+                          
+                    asteroid.setTint(0xff0000);
+                    
+                    if (!asteroid.blinkTween) {
+                        asteroid.blinkTween = this.tweens.add({
+                            targets: asteroid,
+                            alpha: { from: 1, to: 0.3 },
+                            duration: 300,
+                            yoyo: true,
+                            repeat: -1,
+                            ease: 'Sine.easeInOut'
+                        });
+                    }              
+                } else {
+                    asteroid.impactText?.setColor("#00ff00");
+                    lineColor = 0x00ff00;
+                    g.lineStyle(2, lineColor, 1);
+                }
+
+                // Esquinas (punto de mira)
+                g.strokeLineShape(new Phaser.Geom.Line(x - halfW, y - halfH, x - halfW + size, y - halfH));
+                g.strokeLineShape(new Phaser.Geom.Line(x - halfW, y - halfH, x - halfW, y - halfH + size));
+        
+                g.strokeLineShape(new Phaser.Geom.Line(x + halfW, y - halfH, x + halfW - size, y - halfH));
+                g.strokeLineShape(new Phaser.Geom.Line(x + halfW, y - halfH, x + halfW, y - halfH + size));
+        
+                g.strokeLineShape(new Phaser.Geom.Line(x - halfW, y + halfH, x - halfW + size, y + halfH));
+                g.strokeLineShape(new Phaser.Geom.Line(x - halfW, y + halfH, x - halfW, y + halfH - size));
+        
+                g.strokeLineShape(new Phaser.Geom.Line(x + halfW, y + halfH, x + halfW - size, y + halfH));
+                g.strokeLineShape(new Phaser.Geom.Line(x + halfW, y + halfH, x + halfW, y + halfH - size));
+                
+                g.lineStyle(1, lineColor, 1);
+                g.strokeLineShape(new Phaser.Geom.Line(x, y - halfH, x, y - halfH - 20));
+                g.strokeLineShape(new Phaser.Geom.Line(x, y - halfH - 20, x + 20, y - halfH - 20));
+
+
+                // Texto con impactos restantes
+                if (asteroid.impactText) {
+                    asteroid.impactText.setPosition(x + 80, y - halfH - 20);                   
+                    asteroid.impactText.setText("[Impacts:" + (5 - asteroid.impactsLeft) + "/5]");
+                }
+            }
+        });
+        
     }
 }
